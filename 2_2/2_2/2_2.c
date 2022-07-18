2c3473777630f02e00e9f507cd0bdcae01a10cda
TuBR <109275627+TuBR@users.noreply.github.com>
2022-07-18 17:22:42 +0800

    Add files via upload

---
+++ 2_2/2_2/2_2.c
@@ -0,0 +1,13 @@
+// fathm_ft.c -- °Ñ2Ó¢Ñ°×ª»»³ÉÓ¢³ß
+#include<stdio.h>;
+int main()
+{
+	int feet, fathoms;
+
+	fathoms = 2;
+	feet = fathoms * 6;
+	printf("There are %d feet in %d fathoms!\n",feet,fathoms);
+	printf("Yes,I said %d feet",fathoms*6);
+	
+	return 0;
+}
